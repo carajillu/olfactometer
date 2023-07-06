aab5302b0afd4ab2f0a8d6931ccec3b50adc5824
Joan Clark Nicolas <joan.clark88@gmail.com>
2023-07-06 12:06:57 +0800

    Added module olfactometer and it loads all submodules automatically

---
+++ olfactometer/__init__.py
@@ -0,0 +1,24 @@
+import serial
+import time
+
+"""
+The following code will import all files in the folder
+There has to be a simpler way to do it, but this works
+"""
+from inspect import isclass
+from pkgutil import iter_modules
+from pathlib import Path
+from importlib import import_module
+
+# iterate through the modules in the current package
+package_dir = Path(__file__).resolve().parent
+for (_, module_name, _) in iter_modules([package_dir]):
+
+    # import the module and iterate through its attributes
+    module = import_module(f"{__name__}.{module_name}")
+    for attribute_name in dir(module):
+        attribute = getattr(module, attribute_name)
+
+        if isclass(attribute):            
+            # Add the class to this package's variables
+            globals()[attribute_name] = attribute
