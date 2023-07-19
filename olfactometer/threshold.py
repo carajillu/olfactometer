695d72258628846075ca458c6b36366bdafec827
Joan Clark Nicolas <joan.clark88@gmail.com>
2023-07-19 09:27:25 +0700

    Added port number as a parameter. Fixed clean.py Added threshold.py

---
+++ olfactometer/threshold.py
@@ -0,0 +1,52 @@
+import serial
+import time
+import argparse
+
+"""
+Open channel 0 (connstant flow), then open and close channels 1 to 13 (odorants) in succession.
+Open channel 0
+    Open channel 1 for 10 sec, then close
+    Wait 10 sec
+    Open channel 2 for 10 sec, then close
+    Wait 10 sec
+    And so o
+Close channel 0
+"""
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p',"--port",help="Name of port where the instrument is connected (default=com3)",default="com3")
+
+    args = parser.parse_args()
+    return args
+def threshold(port):
+    ser = serial.Serial(port) #defines the port that will be used (how do we know that?)
+    ser.boudrate = 9600 #sets the speed at which data will be transferred to ARDUINO (default?)
+    ser.flush()
+    
+    # Open constant flow
+    ser.write(b'setchannel 0\r') #set to channel 0 (constant flow)
+    ser.write(b'setflow 0:10;\r') # set flow of channel 0 to 10 SPLM (too much?)
+    ser.write(b'setvalve 1\r') # set valve to 1 (open)
+    flow=0.1 # odorant channel flows will be 0.1 SPLM (needs to be negligible in comparison to constant flow)
+    for i in range(1,13): # i is the channel number
+        channel_str="setchannel "+str(i)+"\r"
+        flow_str="setflow "+str(i)+":"+str(flow)+";\r"
+        ser.write(bytes(channel_str)) # set channel
+        ser.write(bytes(flow_str)) # set flow
+        ser.write(b'setvalve 1\r') # open
+        time.sleep(10) # wait
+        ser.write(b'setvalve 0\r') # close 
+        time.sleep(10) # wait
+    
+    #close constant flow
+    ser.write(b'setchannel 0\r') #set to channel 0 (constant flow)
+    ser.write(b'setvalve 0\r') # set valve to 1 (open)
+    ser.close()
+ 
+if __name__=="__main__":
+    print("Executing script threshold.py on its own.")
+    args=parse()
+    port=args.port
+else:
+    print("Importing script threshold.py as "+__name__)
+threshold(port)
--- olfactometer_main.py
+++ olfactometer_main.py
@@ -4,6 +4,7 @@ import argparse
 def parse():
     parser = argparse.ArgumentParser()
     parser.add_argument('-e',"--experiment",help="name of the experiment to run")
+    parser.add_argument('-p',"--port",help="Name of port where the instrument is connected (default=com3)",default="com3")
 
     args = parser.parse_args()
     return args
@@ -11,4 +12,4 @@ def parse():
 if __name__=="__main__":
     args=parse()
     expt=getattr(olfactometer,args.experiment) # gets the module in the olfactometer folder
-    expt()
+    expt(args.port)
